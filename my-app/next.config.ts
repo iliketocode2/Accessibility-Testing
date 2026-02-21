7776dae9cad6c3708ec47d863bc05a944fd56b85
Zachary Klein <zachkklein@gmail.com>
2026-02-21 15:24:14 -0500

    lets try again

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Generate static HTML files for accessibility testing
 };
 
 export default nextConfig;
